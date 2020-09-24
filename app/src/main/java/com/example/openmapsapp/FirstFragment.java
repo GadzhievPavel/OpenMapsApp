70999c4107f8746b32469a63d94e6effd53604cd
GadzhievPavel <Gadzhievpasha@gmail.com>
2020-09-24 16:58:11 +0300

    My location

--- app/src/main/java/com/example/openmapsapp/FirstFragment.java
+++ app/src/main/java/com/example/openmapsapp/FirstFragment.java
@@ -2,11 +2,8 @@ package com.example.openmapsapp;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.pm.PackageManager;
-import android.content.res.Resources;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,41 +13,29 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.ImageButton;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
-
-import com.example.openmapsapp.data.Feature;
-import com.example.openmapsapp.data.Geometry;
 import com.example.openmapsapp.data.Place;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
-import org.osmdroid.tileprovider.tilesource.ITileSource;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.BoundingBox;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.CustomZoomButtonsController;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.Marker;
-import org.osmdroid.views.overlay.OverlayManager;
-import org.osmdroid.views.overlay.infowindow.InfoWindow;
-import org.osmdroid.views.overlay.infowindow.MarkerInfoWindow;
 
+import java.security.Provider;
 import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 import static android.content.Context.LOCATION_SERVICE;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements View.OnClickListener {
 
     private MapView mapView;
     private final String TAG = "Map";
@@ -60,8 +45,7 @@ public class FirstFragment extends Fragment {
     private LocationManager locationManager;
     Place places;
     private IMapController mapController;
-    private GeoPoint point;
-    private Marker marker;
+    private ImageButton myLocation;
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -77,56 +61,44 @@ public class FirstFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         Configuration.getInstance().load(getContext(), PreferenceManager.getDefaultSharedPreferences(getContext()));
         mapView = view.findViewById(R.id.map_view);
-        view.findViewById(R.id.bubble_image);
-        mapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        setupMap();
+        myLocation = view.findViewById(R.id.my_position);
+        myLocation.setOnClickListener(this);
+        requestPermissionsIfNecessary(new String[] {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION});
+        findBestPosition();
+//        MapService.getInstance().getMapAPI().search("54, улица Стройкова, Сысоево, Железнодорожный район, Рязань, Рязанская область, Центральный федеральный округ, 390026, Россия").enqueue(new Callback<Place>() {
+//            @Override
+//            public void onResponse(Call<Place> call, Response<Place> response) {
+//                places = response.body();
+//                for (Feature feature : places.getFeatures()) {
+//                    Geometry geometry = feature.getGeometry();
+//                    point = new GeoPoint(geometry.getCoordinates().get(1).doubleValue(),geometry.getCoordinates().get(0).doubleValue());
+//                    mapController.setCenter(point);
+//                    marker.setPosition(point);
+//                    marker.setTitle("Это здесь");
+//                    mapView.getOverlays().add(marker);
+//                }
+//            }
+//            @Override
+//            public void onFailure(Call<Place> call, Throwable t) {
+//                Log.e("MAPTEST",t.getMessage());
+//            }
+//        });
+
+
+    }
+
+    private void setupMap(){
+        mapView.setTileSource(TileSourceFactory.MAPNIK);
         mapView.setMultiTouchControls(true);
         mapView.getZoomController().setVisibility(CustomZoomButtonsController.Visibility.NEVER);
         mapView.setTilesScaledToDpi(true);
         mapView.setMinZoomLevel(4.0);
         mapView.setMaxZoomLevel(19.0);
-        requestPermissionsIfNecessary(new String[] {
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-        });
         mapController = mapView.getController();
         mapController.setZoom(18.0);
-        MapService.getInstance().getMapAPI().search("54, улица Стройкова, Сысоево, Железнодорожный район, Рязань, Рязанская область, Центральный федеральный округ, 390026, Россия").enqueue(new Callback<Place>() {
-            @Override
-            public void onResponse(Call<Place> call, Response<Place> response) {
-                places = response.body();
-                for (Feature feature : places.getFeatures()) {
-                    Geometry geometry = feature.getGeometry();
-                    point = new GeoPoint(geometry.getCoordinates().get(1).doubleValue(),geometry.getCoordinates().get(0).doubleValue());
-                    mapController.setCenter(point);
-                    marker.setPosition(point);
-                    marker.setTitle("Это здесь");
-                    mapView.getOverlays().add(marker);
-                }
-            }
-            @Override
-            public void onFailure(Call<Place> call, Throwable t) {
-                Log.e("MAPTEST",t.getMessage());
-            }
-        });
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 10, 10, locationListener);
-        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,1000*10,10, locationListener);
-        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        point = new GeoPoint(54.9610646, 41.7551758);
-        mapController.setCenter(point);
-        marker = new Marker(mapView);
-        marker.setPosition(point);
-        marker.setTitle("Вы тут");
-        mapView.getOverlays().add(marker);
     }
 
-    private LocationListener locationListener = new LocationListener() {
-        @Override
-        public void onLocationChanged(@NonNull Location location) {
-                  Log.e(TAG+"Location","latitude "+location.getLatitude()+" longitude "+ location.getLongitude());
-        }
-    };
     @Override
     public void onResume() {
         super.onResume();
@@ -161,6 +133,38 @@ public class FirstFragment extends Fragment {
                     getActivity(),
                     permissionsToRequest.toArray(new String[0]),
                     REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+    private LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(@NonNull Location location) {
+            Log.e(TAG+"Location","latitude "+location.getLatitude()+" longitude "+ location.getLongitude());
+            GeoPoint point = new GeoPoint(location.getLatitude(), location.getLongitude());
+            Log.e(TAG+"Location",point.toString());
+            mapController.setCenter(point);
+            Marker marker = new Marker(mapView);
+            marker.setPosition(point);
+            marker.setTitle("Вы тут");
+            mapView.getOverlays().add(marker);
+        }
+    };
+
+    @SuppressLint({"WrongConstant", "MissingPermission"})
+    private void findBestPosition(){
+//        Criteria criteria = new Criteria();
+//        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+//        criteria.setCostAllowed(false);
+//        String provider = locationManager.getBestProvider(criteria,false);
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 100 * 10, 5, locationListener);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,100 * 10, 5, locationListener);
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.my_position:
+                findBestPosition();
+                break;
         }
     }
 }
